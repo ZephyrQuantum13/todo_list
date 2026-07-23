e28f282ffce9f2fb4e38a9bff425bddf496732b0
Vladim2393 <vova132023111@gmail.com>
2026-07-23 15:12:10 +0500

    feat: add endpoint to retrieve all tasks

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+import uvicorn
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return{"message":"Hello World!"}
+class TaskSchema(BaseModel):
+    id: int
+    title: str
+    description: str | None = None
 
+tasks: list[TaskSchema] = []
+
+
+
+@app.get("/tasks")
+def read_tasks() -> list[TaskSchema]:
+    return tasks
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
